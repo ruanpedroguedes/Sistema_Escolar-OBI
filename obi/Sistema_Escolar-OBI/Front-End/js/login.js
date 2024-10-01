9099728f44a71ce402d8886d8acbe830f30f833c
ruanpedroguedes <ruanpedro9999@gmail.com>
2024-10-01 13:51:52 -0300

    tela de bem sucedido feita

--- obi/Sistema_Escolar-OBI/Front-End/js/login.js
+++ obi/Sistema_Escolar-OBI/Front-End/js/login.js
@@ -1,18 +1,14 @@
 const API_URL = 'http://localhost:3000/api/cadastroRoute/register';
 
-
 document.getElementById('complaintForm').addEventListener('submit', async (e) => {
     e.preventDefault();
 
     // Capturando os valores dos campos
     const username = document.getElementById('nome').value;
     const useremail = document.getElementById('email').value;
-    const usertype = document.querySelector('.selected-option span').innerText
-    // const cargo = cargoElement ? cargoElement.innerText : ''; 
-
+    const usertype = document.querySelector('.selected-option span').innerText;
     const password = document.querySelector('input[name="password"]').value; 
     const confirmarSenha = document.querySelectorAll('input[name="password"]')[1].value; 
-   
 
     // Enviando os dados para a API
     const response = await fetch(API_URL, {
@@ -20,7 +16,7 @@ document.getElementById('complaintForm').addEventListener('submit', async (e) =>
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ username, useremail,  password, usertype })
+        body: JSON.stringify({ username, useremail, password, usertype })
     });
 
     // Verificando se o cadastro foi bem-sucedido
@@ -35,14 +31,20 @@ document.getElementById('complaintForm').addEventListener('submit', async (e) =>
         document.querySelector('input[name="password"]').value = '';
         document.querySelectorAll('input[name="password"]')[1].value = '';
 
-        // Você pode adicionar um feedback ao usuário aqui, como uma mensagem de sucesso
-        alert('Cadastro realizado com sucesso!');
+         // Mostrar a mensagem de sucesso
+         messageCard.classList.remove('hidden');
+        
+         // Após 2 segundos, esconder a mensagem
+         
+        // Manter a mensagem visível por 2 segundos antes de redirecionar
+        setTimeout(() => {
+            messageCard.classList.add('hidden');
+            window.location.href = 'login.html'; // Redirecionar após esconder a mensagem
+        }, 4000);
     } else {
         // Tratar erro, caso a resposta não seja ok
         const error = await response.json();
         console.error('Erro no cadastro:', error);
         alert('Erro ao cadastrar. Tente novamente.');
     }
-
 });
-
