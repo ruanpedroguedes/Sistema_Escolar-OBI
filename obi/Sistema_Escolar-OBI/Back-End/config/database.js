9695d358b307eb48401c8f77668dba6d6863eff1
ruanpedroguedes <ruanpedro9999@gmail>
2024-10-03 13:22:28 -0300

    tela de perfil professor atualizada

--- obi/Sistema_Escolar-OBI/Back-End/config/database.js
+++ obi/Sistema_Escolar-OBI/Back-End/config/database.js
@@ -13,4 +13,4 @@ const connectDB = async () => {
    }       
 }
 
-module.exports = connectDB
+module.exports = connectDB;
--- obi/Sistema_Escolar-OBI/Back-End/models/userModel.js
+++ obi/Sistema_Escolar-OBI/Back-End/models/userModel.js
@@ -1,23 +1,24 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
-   username: {type: String, required: true, unique: true},
-   useremail: {type: String, required: true, unique: true},
-   password: { type: String, required: true },
-   usertype: { type: String, enum: ['Aluno', 'Professor', 'Coordenação'], required: true }       
-})
+    username: { type: String, required: true, unique: true },
+    useremail: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    usertype: { type: String, enum: ['Aluno', 'Professor', 'Coordenação'], required: true }
+});
 
+// Hook para hash da senha antes de salvar
 UserSchema.pre('save', async function (next) {
     if (this.isModified('password')) {
-       this.password = await bcrypt.hash(this.password, 10)   
-    }      
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+});
 
-    next()
-})
+// Método para comparar senhas
+UserSchema.methods.comparePassword = async function(password) {
+    return await bcrypt.compare(password, this.password);
+};
 
-UserSchema.methods.comparePassword = function(password) {
-   return bcrypt.compare(password, this.password)       
-}
-
-module.exports = mongoose.model("User", UserSchema)
+module.exports = mongoose.model("User", UserSchema);
